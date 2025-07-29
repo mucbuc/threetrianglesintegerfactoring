8983e5a2fa8c5aa85f6978b508807b18f189c807
mucbuc <mucbuc@mucbucs-Mac-mini-2.local>
2025-07-28 23:00:33 -0700

    html output

--- doc_example_run.js
+++ doc_example_run.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-let [, , semiprime] = process.argv;
+let [, , semiprime, html] = process.argv;
 let assert = require('assert');
 
 if (semiprime) {
@@ -37,10 +37,20 @@ function factor(semiprime)
 
         let s = sum(a, b);
         ++index;
-        console.log( `${index}. a = ${a}, b = ${b}: S(${a}, ${b}) = ${s}  `);
+        if (html) {
+            console.log(`<li> S(${a}, ${b}) = ${s}`);
+        } 
+        else {
+            console.log(`${index}. S(${a}, ${b}) = ${s}  `);
+        }
 
         if (s > semiprime) {
-            console.log(`  => a = a - 2, b = b + 2`);
+            if (html) {
+                console.log(`<b>=></b> a = a - 2, b = b + 2</li>`);
+            }
+            else {
+                console.log(`  => a = a - 2, b = b + 2`);
+            }
 
             a -= 2;
             b += 2;
@@ -48,7 +58,12 @@ function factor(semiprime)
         }
 
         if (s < semiprime) {
-            console.log(`  => b = b + max(1, ceil(${semiprime - s} / ${a + 1}))  `);
+            if (html) {
+                console.log(`  <b>=></b> b = b + max(1, ceil(${semiprime - s} / ${a + 1}))</li>`);
+            }
+            else {
+                console.log(`  => b = b + max(1, ceil(${semiprime - s} / ${a + 1}))  `);
+            }
 
             let d = Math.max(1, Math.ceil((semiprime - s) / (a + 1)));
             b += d;
@@ -56,14 +71,26 @@ function factor(semiprime)
         }
 
         if (s == semiprime) {
-            console.log(`  => ${semiprime} = ${a + 1} * ${a + b}  `);
+            if (html) {
+                console.log(`  <b>=></b> ${semiprime} = ${a + 1} * ${a + b}</li>`);
+            }
+            else {
+                console.log(`  => ${semiprime} = ${a + 1} * ${a + b}  `);
+            }
 
             return [ a + 1, a + b ];
         }
     }
 
-    console.log( `${index + 1}. a = ${a}, b = ${b}  `);
-    console.log(`  => ${semiprime} = 1 * ${semiprime}`);
+    if (html) {
+        console.log(`<li> a = ${a}, b = ${b}`);
+        console.log(`  <b>=></b> ${semiprime} = 1 * ${semiprime}</li>`);
+    }
+    else {
+        console.log(`${index + 1}. a = ${a}, b = ${b}  `);
+        console.log(`  => ${semiprime} = 1 * ${semiprime}`);
+    }
+    
 
     return [ 1, semiprime ];
 }
